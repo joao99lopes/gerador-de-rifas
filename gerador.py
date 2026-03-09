e7cdedc26b203e3450ace8398e625954e0c69eb4
Joao Lopes <joao.aclopes99@gmail.com>
2026-03-09 13:53:08 +0000

    melhorias

--- gerador.py
+++ gerador.py
@@ -1,11 +1,13 @@
+import math
 import os
 
 from PIL import Image
 from reportlab.lib.pagesizes import A4, landscape
+from reportlab.lib.utils import ImageReader
 from reportlab.pdfgen import canvas
 
 # CONFIGURAÇÕES
-TOTAL_RIFAS = 1100
+TOTAL_RIFAS = 1000
 RIFAS_POR_LINHAS = 2
 RIFAS_POR_COLUNA = 4
 IMAGEM_RIFA = "template.png"  # <-- mete aqui o nome do teu ficheiro
@@ -15,35 +17,33 @@ BUILD_DIR = os.path.join(BASE_DIR, "build")
 os.makedirs(BUILD_DIR, exist_ok=True)
 
 OUTPUT = os.path.join(BUILD_DIR, f"rifas_1_a_{TOTAL_RIFAS}.pdf")
-
-# Abrir imagem base
+img_reader = ImageReader(IMAGEM_RIFA)
 img = Image.open(IMAGEM_RIFA)
 img_width, img_height = img.size
 
-# Página A4 horizontal
 page_width, page_height = landscape(A4)
 
-# Calcular tamanho de cada rifa (4 colunas x 2 linhas)
 rifa_width = page_width / RIFAS_POR_LINHAS
 rifa_height = page_height / RIFAS_POR_COLUNA
 
 c = canvas.Canvas(OUTPUT, pagesize=landscape(A4))
 
-numero_atual = 1
+rifas_por_pagina = RIFAS_POR_LINHAS * RIFAS_POR_COLUNA
+total_paginas = math.ceil(TOTAL_RIFAS / rifas_por_pagina)
 
-while numero_atual <= TOTAL_RIFAS:
-    for linha in range(4):
-        for coluna in range(2):
+for pagina in range(total_paginas):
+    posicao = 0
+
+    for linha in range(RIFAS_POR_COLUNA):
+        for coluna in range(RIFAS_POR_LINHAS):
+            numero_atual = pagina + (posicao * total_paginas) + 1
             if numero_atual > TOTAL_RIFAS:
-                break
-
+                continue
             x = coluna * rifa_width
             y = page_height - ((linha + 1) * rifa_height)
 
             # Desenhar imagem
-            c.drawImage(IMAGEM_RIFA, x, y, width=rifa_width, height=rifa_height)
-
-            # Adicionar número (ajusta posição se necessário)
+            c.drawImage(img_reader, x, y, width=rifa_width, height=rifa_height)
             c.setFont("Helvetica", 10)
 
             # Número a 90º
@@ -53,9 +53,10 @@ while numero_atual <= TOTAL_RIFAS:
             c.drawString(0, 0, f"#{numero_atual:04d}")
             c.restoreState()
 
-            # Número normal (0º)
+            # Número normal
             c.drawString(x + 370, y + 130, f"#{numero_atual:04d}")
-            numero_atual += 1
+
+            posicao += 1
 
     c.showPage()
 
